26b7f26f1821014ae701ed3feff716dff7f44595
zlyuancn <zlyuancn@163.com>
2021-09-06 18:09:28 +0800

    完善dom的方法

--- core/dom/dom.go
+++ core/dom/dom.go
@@ -9,50 +9,84 @@ import (
 )
 
 type Dom struct {
-	*html.Node
+	node      *html.Node
+	Type      html.NodeType
+	Data      string
+	Namespace string
+	Attr      []html.Attribute
+}
+
+// 返回原始html.node
+func (d *Dom) RawNode() *html.Node {
+	return d.node
 }
 
 // xpath查找所有, 表达式错误会panic
 func (d *Dom) Xpath(expr string) []*Dom {
-	nodes := htmlquery.Find(d.Node, expr)
+	nodes := htmlquery.Find(d.node, expr)
 	return makeDom(nodes)
 }
 
 // xpath查找一个, 表达式错误会panic
 func (d *Dom) XpathOne(expr string) *Dom {
-	node := htmlquery.FindOne(d.Node, expr)
+	node := htmlquery.FindOne(d.node, expr)
 	return makeOneDom(node)
 }
 
 // css查找所有, 表达式错误会panic
 func (d *Dom) Css(expr string) []*Dom {
 	sel := getCssQuery(expr)
-	nodes := cascadia.QueryAll(d.Node, sel)
+	nodes := cascadia.QueryAll(d.node, sel)
 	return makeDom(nodes)
 }
 
 // css查找一个, 表达式错误会panic
 func (d *Dom) CssOne(expr string) *Dom {
 	sel := getCssQuery(expr)
-	node := cascadia.Query(d.Node, sel)
+	node := cascadia.Query(d.node, sel)
 	return makeOneDom(node)
 }
 
 // 获取属性
 func (d *Dom) GetAttr(name string) string {
-	return htmlquery.SelectAttr(d.Node, name)
+	return htmlquery.SelectAttr(d.node, name)
 }
 
 // 获取node内所有的文本值
 func (d *Dom) InnerText() string {
-	return htmlquery.InnerText(d.Node)
+	return htmlquery.InnerText(d.node)
 }
 
 /*将node转为html
   self 表示是否输入自己
 */
-func (d *Dom) HTML(self bool) string {
-	return htmlquery.OutputHTML(d.Node, self)
+func (d *Dom) OutputHTML(self bool) string {
+	return htmlquery.OutputHTML(d.node, self)
+}
+
+// 返回上级节点
+func (d *Dom) Parent() *Dom {
+	return makeOneDom(d.node.Parent)
+}
+
+// 返回第一个子节点
+func (d *Dom) FirstChild() *Dom {
+	return makeOneDom(d.node.FirstChild)
+}
+
+// 返回最后一个子节点
+func (d *Dom) LastChild() *Dom {
+	return makeOneDom(d.node.LastChild)
+}
+
+// 返回上一个同级节点
+func (d *Dom) PrevSibling() *Dom {
+	return makeOneDom(d.node.PrevSibling)
+}
+
+// 返回下一个同级节点
+func (d *Dom) NextSibling() *Dom {
+	return makeOneDom(d.node.NextSibling)
 }
 
 func NewDom(r io.Reader) (*Dom, error) {
@@ -66,11 +100,20 @@ func NewDom(r io.Reader) (*Dom, error) {
 func makeDom(nodes []*html.Node) []*Dom {
 	dom := make([]*Dom, len(nodes))
 	for i, node := range nodes {
-		dom[i] = &Dom{node}
+		dom[i] = makeOneDom(node)
 	}
 	return dom
 }
 
 func makeOneDom(node *html.Node) *Dom {
-	return &Dom{node}
+	if node == nil {
+		return nil
+	}
+	return &Dom{
+		node:      node,
+		Type:      node.Type,
+		Data:      node.Data,
+		Namespace: node.Namespace,
+		Attr:      node.Attr,
+	}
 }
--- core/dom/xml_dom.go
+++ core/dom/xml_dom.go
@@ -7,19 +7,72 @@ import (
 )
 
 type XmlDom struct {
-	*xmlquery.Node
+	node *xmlquery.Node
+
+	Type         xmlquery.NodeType
+	Data         string
+	Prefix       string
+	NamespaceURI string
+	Attr         []xmlquery.Attr
+}
+
+// 返回原始xmlquery.node
+func (d *XmlDom) RawNode() *xmlquery.Node {
+	return d.node
 }
 
 // 查找所有, 表达式错误会panic
 func (d *XmlDom) XmlXpath(expr string) []*XmlDom {
-	nodes := xmlquery.Find(d.Node, expr)
+	nodes := xmlquery.Find(d.node, expr)
 	return makeXmlDom(nodes)
 }
 
 // 查找一个, 表达式错误会panic
 func (d *XmlDom) XmlXpathOne(expr string) *XmlDom {
-	node := xmlquery.FindOne(d.Node, expr)
+	node := xmlquery.FindOne(d.node, expr)
 	return makeOneXmlDom(node)
+}
+
+// 获取属性
+func (d *XmlDom) GetAttr(name string) string {
+	return d.node.SelectAttr(name)
+}
+
+// 获取node内所有的文本值
+func (d *XmlDom) InnerText() string {
+	return d.node.InnerText()
+}
+
+/*将node转为xml
+  self 表示是否输入自己
+*/
+func (d *XmlDom) OutputXML(self bool) string {
+	return d.node.OutputXML(self)
+}
+
+// 返回上级节点
+func (d *XmlDom) Parent() *XmlDom {
+	return makeOneXmlDom(d.node.Parent)
+}
+
+// 返回第一个子节点
+func (d *XmlDom) FirstChild() *XmlDom {
+	return makeOneXmlDom(d.node.FirstChild)
+}
+
+// 返回最后一个子节点
+func (d *XmlDom) LastChild() *XmlDom {
+	return makeOneXmlDom(d.node.LastChild)
+}
+
+// 返回上一个同级节点
+func (d *XmlDom) PrevSibling() *XmlDom {
+	return makeOneXmlDom(d.node.PrevSibling)
+}
+
+// 返回下一个同级节点
+func (d *XmlDom) NextSibling() *XmlDom {
+	return makeOneXmlDom(d.node.NextSibling)
 }
 
 func NewXmlDom(r io.Reader) (*XmlDom, error) {
@@ -33,11 +86,18 @@ func NewXmlDom(r io.Reader) (*XmlDom, error) {
 func makeXmlDom(nodes []*xmlquery.Node) []*XmlDom {
 	dom := make([]*XmlDom, len(nodes))
 	for i, node := range nodes {
-		dom[i] = &XmlDom{node}
+		dom[i] = makeOneXmlDom(node)
 	}
 	return dom
 }
 
 func makeOneXmlDom(node *xmlquery.Node) *XmlDom {
-	return &XmlDom{node}
+	return &XmlDom{
+		node:         node,
+		Type:         node.Type,
+		Data:         node.Data,
+		Prefix:       node.Prefix,
+		NamespaceURI: node.NamespaceURI,
+		Attr:         node.Attr,
+	}
 }
