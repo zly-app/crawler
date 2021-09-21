cbdd9b4fd7d6d0d391014cedfee91972592b0e56
zlyuancn <zlyuancn@163.com>
2021-09-21 11:33:34 +0800

    spider_base.ini改为spider_config.ini

--- tools/crawler/cmd.init.go
+++ tools/crawler/cmd.init.go
@@ -21,7 +21,7 @@ func CmdInit(context *cli.Context) error {
 
 	MustMkdir(fmt.Sprintf("%s/configs", projectName))
 	MustWriteFile(fmt.Sprintf("%s/configs/scheduler.toml", projectName), MustReadEmbedFile("template/scheduler.toml"))
-	MustWriteFile(fmt.Sprintf("%s/configs/spider_base.toml", projectName), MustReadEmbedFile("template/spider_base.toml"))
+	MustWriteFile(fmt.Sprintf("%s/configs/spider_config.toml", projectName), MustReadEmbedFile("template/spider_config.toml"))
 	MustWriteFile(fmt.Sprintf("%s/configs/supervisor_spider_config.ini", projectName), MustReadEmbedFile("template/supervisor_spider_config.ini"))
 
 	MustMkdir(fmt.Sprintf("%s/spiders", projectName))
