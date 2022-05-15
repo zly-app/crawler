8e0292ffb390b285a75dbbd77497e303417953ba
zlyuancn <zlyuancn@163.com>
2022-05-15 18:11:57 +0800

    添加一些配置示例

--- config/frame.go
+++ config/frame.go
@@ -43,15 +43,20 @@ const (
 )
 
 type FrameConfig struct {
-	QueueSuffixes              []string // 队列后缀, 按顺序查找种子
-	SeedQueueSuffix            string   // 种子队列后缀名
-	ErrorSeedQueueSuffix       string   // 错误种子队列后缀名, 主要是下载器错误
-	ParserErrorSeedQueueSuffix string   // 解析错误种子队列后缀名, 包括处理程序错误, 处理程序不存在, 种子解析失败等
-	SetSuffix                  string   // 集合后缀名
+	QueueSuffixes        []string // 队列后缀, spider运行时按顺序在队列中取出种子
+	SeedQueueSuffix      string   // 种子队列后缀名, 提交种子时会放在这个队列
+	ErrorSeedQueueSuffix string   // 错误种子队列后缀名, 下载器错误和请求中间件拦截以及响应中间件拦截时会放入这个队列
+	// 解析错误种子队列后缀名, 包括处理程序错误或panic, 处理程序不存在, 种子解析失败错误会放入这个队列.
+	// 在这个队列的种子一般需要人工干预查看原因, 类似mq的死信队列.
+	ParserErrorSeedQueueSuffix string // 解析错误种子队列后缀名, 包括处理程序错误, 处理程序不存在, 种子解析失败等
+	SetSuffix                  string // 集合后缀名
 
 	StopSubmitInitialSeedIfNotEmptyQueue bool // 非空队列不提交初始化种子
-	CheckEmptyQueueIgnoreErrorQueue      bool // 检查是否为空队列的程序忽略error队列
-	SubmitSeedToQueueFront               bool // 提交种子到队列前面, (取出种子是从前面开始)
+	CheckEmptyQueueIgnoreErrorQueue      bool // 检查是否为空队列时忽略error队列的检查, 一般会在提交初始化种子信号时检查
+	// 控制提交种子到队列前面. 取出种子一定是从前面开始的.
+	// 如果设为 true, 表示后进先出(深度优先).
+	// 如果设为 false, 表示先进先出(广度优先).
+	SubmitSeedToQueueFront bool
 
 	RequestTimeout         int64 // 请求超时, 单位毫秒
 	NextSeedWaitTime       int64 // 下一个seed等待时间, 单位毫秒
